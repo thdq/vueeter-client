d1b1108974c8b9aba2e43e15facd12b2ad0f5fb1
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-17 23:45:25 -0300

    feat: ensure RemoteAuthentication throws if HttpPostClient returns 404 on UnexpectedError

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -2,7 +2,8 @@ export enum HttpStatusCode {
     success = 200,
     noContent = 204,
     badRequest = 400,
-    unathorized = 401
+    unathorized = 401,
+    notFound = 404
 }
 
 export type HttpResponse = {
