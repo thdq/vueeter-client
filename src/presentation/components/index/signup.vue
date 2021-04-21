5c27eb3f753515a655b9f54c77665aa23909e145
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-21 01:49:08 -0300

    feat: ensure v-signup component exists inside SignUp modal

---
+++ src/presentation/components/index/signup.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    name: 'v-signup',
+    data () {
+        return {
+
+        }
+    }
+})
+</script>
