1ea0b0190fca8cb641aa8ea88fa7cbb070b1b0b0
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-04-28 21:59:01 -0300

    feat: ensure RemoteAuthentication calls Validation with correct values

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,17 +3,24 @@ import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials'
 import { UnexpectedError } from '@/domain/errors/unexpected'
 import { UserModel } from '@/domain/model/user-model'
 import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
+import { Validation } from '@/presentation/protocols/validation'
 
 export class RemoteAuthentication implements Authentication {
     private readonly url: string
     private readonly httpClient: HttpClient<UserModel>
+    private readonly validation: Validation
 
-    constructor (url: string, httpClient: HttpClient<UserModel>) {
+    constructor (url: string, httpClient: HttpClient<UserModel>, validation: Validation) {
         this.url = url
         this.httpClient = httpClient
+        this.validation = validation
     }
 
     async auth (params: AuthenticationParams): Promise<UserModel> {
+
+        const validationError = this.validation.validate(params)
+
+        if (validationError) throw new Error(validationError.message)
 
         const httpResponse = await this.httpClient.request({
             url: this.url,
--- src/main/factories/validation/login.ts
+++ src/main/factories/validation/login.ts
@@ -1,15 +1,15 @@
+import { ValidationComposite, RequiredFieldValidation } from "@/presentation/protocols/validation/validators"
 import { Validation } from "@/presentation/protocols/validation"
-import { RequiredFieldValidation } from "@/presentation/protocols/validation/validators/required-field-validation"
-import { ValidationComposite } from "@/presentation/protocols/validation/validators/validation-composite"
 
 export const makeLoginValidation = (): ValidationComposite => {
 
     const validations: Validation[] = []
 
     for (const field of ['username', 'password']) {
+
         validations.push(new RequiredFieldValidation(field))
+
     }
 
     return new ValidationComposite(validations)
-
 }
