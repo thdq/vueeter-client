2230f417f08395b9092cf2bec7e80b6e5f69d618
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-05-05 22:21:08 -0300

    test: ensure SignUp component renders form with signup params

--- src/presentation/components/index/signup/signup.spec.ts
+++ src/presentation/components/index/signup/signup.spec.ts
@@ -31,4 +31,24 @@ describe('SignUp component', () => {
 
     })
 
+    test('Should render form with signup params', () => {
+
+        const { wrapper } = makeSut()
+
+        const nameInput = wrapper.find('[data-test=name-input]')
+        const emailInput = wrapper.find('[data-test=email-input]')
+        const passwordInput = wrapper.find('[data-test=password-input]')
+        const passwordConfirmInput = wrapper.find('[data-test=passwordConfirm-input]')
+        const birthDateInput = wrapper.find('[data-test=birthDate-input]')
+        const usernameInput = wrapper.find('[data-test=username-input]')
+
+        expect(nameInput.exists()).toBe(true)
+        expect(emailInput.exists()).toBe(true)
+        expect(passwordInput.exists()).toBe(true)
+        expect(passwordConfirmInput.exists()).toBe(true)
+        expect(birthDateInput.exists()).toBe(true)
+        expect(usernameInput.exists()).toBe(true)
+
+    })
+
 })
