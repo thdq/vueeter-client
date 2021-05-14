c9eaf5db3c10e3dac812540298b0b7a67ec4a949
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-05-13 21:33:28 -0300

    feat: apply validation in signup form with vee-validate

--- src/presentation/components/index/signup/signup.vue
+++ src/presentation/components/index/signup/signup.vue
@@ -1,61 +1,126 @@
 <template>
     <div>
-        <form @submit.prevent="handle(form, apiResponse)">
-            <vs-row align="center" justify="space-around" direction="column">
-                <vs-col w="12">
-                    <vs-row class="py-4">
-                        <vs-input v-model="form.name" :label-placeholder="$t('welcome.signup.form.label.name')" block data-test="name-input" />
-                    </vs-row>
-                    <vs-row class="py-4">
-                        <vs-input v-model="form.email" :label-placeholder="$t('welcome.signup.form.label.email')" block data-test="email-input" />
-                    </vs-row>
-                    <vs-row class="py-4">
-                        <vs-input v-model="form.birth_date" type="date" :label-placeholder="$t('welcome.signup.form.label.birth_date')" block data-test="birthDate-input" />
-                    </vs-row>
-                    <vs-row class="py-4">
-                        <vs-input v-model="form.password" type="password" :label-placeholder="$t('welcome.signup.form.label.password')" block data-test="password-input" />
-                    </vs-row>
-                    <vs-row class="py-4">
-                        <vs-input v-model="form.passwordConfirm" type="password" :label-placeholder="$t('welcome.signup.form.label.passwordConfirm')" block data-test="passwordConfirm-input" />
-                    </vs-row>
-                    <vs-row class="py-4">
-                        <vs-input v-model="form.username" :label-placeholder="$t('welcome.signup.form.label.username')" block data-test="username-input" />
-                    </vs-row>
-                    <vs-row v-if="apiResponse.error">
-                        <vs-alert class="alert-error" data-test="alert-error" color="danger">
-                            <template #icon>
-                                <i class="bx bx-error-circle" />
-                            </template>
-                            <template class="py-0" #title>
-                                {{ $t('welcome.signup.form.error.title') }}
-                            </template>
-                            {{ apiResponse.messageError }}
-                        </vs-alert>
-                    </vs-row>
-                    <vs-row class="py-2">
-                        <vs-button
-                            data-test="signup-button"
-                            :loading="apiResponse.waiting"
-                            block
-                            border
-                            flat
-                            @click="handle(form, apiResponse)"
-                        >
-                            {{ $t('welcome.signup.form.buttons.create_account') }}
-                        </vs-button>
-                    </vs-row>
-                </vs-col>
-            </vs-row>
-        </form>
+        <ValidationObserver v-slot="{ invalid }">
+            <form @submit.prevent="handle(form, apiResponse, invalid)">
+                <vs-row align="center" justify="space-around" direction="column">
+                    <vs-col w="12">
+                        <vs-row class="py-4">
+                            <ValidationProvider v-slot="{ errors }" class="w-full" name="name" rules="required">
+                                <vs-input v-model="form.name" :label-placeholder="$t('welcome.signup.form.label.name')" block data-test="name-input" :danger="errors.length > 0">
+                                    <template v-if="errors.length > 0" #message-danger>
+                                        {{ errors[0] }}
+                                    </template>
+                                </vs-input>
+                            </ValidationProvider>
+                        </vs-row>
+                        <vs-row class="py-4">
+                            <ValidationProvider v-slot="{ errors }" class="w-full" name="email" rules="required|email">
+                                <vs-input v-model="form.email" :label-placeholder="$t('welcome.signup.form.label.email')" block data-test="email-input" :danger="errors.length > 0">
+                                    <template v-if="errors.length > 0" #message-danger>
+                                        {{ errors[0] }}
+                                    </template>
+                                </vs-input>
+                            </ValidationProvider>
+                        </vs-row>
+                        <vs-row class="py-4">
+                            <ValidationProvider v-slot="{ errors }" class="w-full" name="birth_date" rules="required">
+                                <vs-input
+                                    v-model="form.birth_date"
+                                    type="date"
+                                    :label-placeholder="$t('welcome.signup.form.label.birth_date')"
+                                    block
+                                    data-test="birthDate-input"
+                                    :danger="errors.length > 0"
+                                >
+                                    <template v-if="errors.length > 0" #message-danger>
+                                        {{ errors[0] }}
+                                    </template>
+                                </vs-input>
+                            </ValidationProvider>
+                        </vs-row>
+                        <vs-row class="py-4">
+                            <ValidationProvider v-slot="{ errors }" class="w-full" name="password" rules="required|confirmed:confirmation">
+                                <vs-input
+                                    v-model="form.password"
+                                    type="password"
+                                    :label-placeholder="$t('welcome.signup.form.label.password')"
+                                    block
+                                    data-test="password-input"
+                                    :danger="errors.length > 0"
+                                >
+                                    <template v-if="errors.length > 0" #message-danger>
+                                        {{ errors[0] }}
+                                    </template>
+                                </vs-input>
+                            </ValidationProvider>
+                        </vs-row>
+                        <vs-row class="py-4">
+                            <ValidationProvider v-slot="{ errors }" class="w-full" name="passwordConfirm" vid="confirmation" rules="required">
+                                <vs-input
+                                    v-model="form.passwordConfirm"
+                                    type="password"
+                                    :label-placeholder="$t('welcome.signup.form.label.passwordConfirm')"
+                                    block
+                                    data-test="passwordConfirm-input"
+                                    :danger="errors.length > 0"
+                                >
+                                    <template v-if="errors.length > 0" #message-danger>
+                                        {{ errors[0] }}
+                                    </template>
+                                </vs-input>
+                            </ValidationProvider>
+                        </vs-row>
+                        <vs-row class="py-4">
+                            <ValidationProvider v-slot="{ errors }" class="w-full" name="username" rules="required">
+                                <vs-input v-model="form.username" :label-placeholder="$t('welcome.signup.form.label.username')" block data-test="username-input" :danger="errors.length > 0">
+                                    <template v-if="errors.length > 0" #message-danger>
+                                        {{ errors[0] }}
+                                    </template>
+                                </vs-input>
+                            </ValidationProvider>
+                        </vs-row>
+                        <vs-row v-if="apiResponse.error">
+                            <vs-alert class="alert-error" data-test="alert-error" color="danger">
+                                <template #icon>
+                                    <i class="bx bx-error-circle" />
+                                </template>
+                                <template class="py-0" #title>
+                                    {{ $t('welcome.signup.form.error.title') }}
+                                </template>
+                                {{ apiResponse.messageError }}
+                            </vs-alert>
+                        </vs-row>
+                        <vs-row class="py-2">
+                            <vs-button
+                                :disabled="invalid"
+                                data-test="signup-button"
+                                :loading="apiResponse.waiting"
+                                block
+                                border
+                                flat
+                                type="submit"
+                            >
+                                {{ $t('welcome.signup.form.buttons.create_account') }}
+                            </vs-button>
+                        </vs-row>
+                    </vs-col>
+                </vs-row>
+            </form>
+        </ValidationObserver>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { defineComponent, reactive } from '@nuxtjs/composition-api'
+import { ValidationProvider, ValidationObserver } from 'vee-validate/dist/vee-validate.full'
 import { SignUpParams } from "@/domain/usecases/signup"
 import { SignupAPIResponse } from "./signup.protocols"
 
 export default defineComponent({
+    components: {
+        ValidationProvider,
+        ValidationObserver
+    },
     name: 'VSignup',
     setup () {
 
@@ -74,10 +139,11 @@ export default defineComponent({
             username: ""
         })
 
-        const handle = (form: SignUpParams, apiResponse: SignupAPIResponse): void => {
+        const handle = (form: SignUpParams, apiResponse: SignupAPIResponse, invalid: boolean = true): void => {
 
             try {
 
+                if (invalid) return
                 apiResponse.waiting = true
 
             } catch (error) {
