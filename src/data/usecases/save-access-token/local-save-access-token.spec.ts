98a4c738605b3122bd1d2b6f388c1556a5710c3f
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-05-14 23:45:14 -0300

    test: ensure LocalSaveAccessToken calls SetStorage with correct value

---
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -0,0 +1,53 @@
+import { SetStorage } from '@/data/protocols/cache/set-storage'
+import faker from 'faker'
+import { LocalSaveAccessToken } from './local-save-access-token'
+
+interface SutTypes {
+    sut: LocalSaveAccessToken
+    setStorageStub: SetStorage
+}
+
+const makeSetStorage = (): SetStorage => {
+
+    class SetStorageStub implements SetStorage {
+        key: string
+        value: any
+
+        async set (key: string, value: string): Promise<void> {
+            this.key = key
+            this.value = value
+            await Promise.resolve()
+        }
+    }
+
+    return new SetStorageStub()
+}
+
+const makeSut = (): SutTypes => {
+
+    const setStorageStub = makeSetStorage()
+    const sut = new LocalSaveAccessToken(setStorageStub)
+
+    return {
+        sut,
+        setStorageStub
+    }
+
+}
+
+describe('LocalSaveAccessToken', () => {
+
+    test('Should call SetStorage with correct value', async () => {
+
+        const { sut, setStorageStub } = makeSut()
+
+        const accessToken = faker.datatype.uuid()
+
+        await sut.save(accessToken)
+
+        expect(setStorageStub.key).toBe('accessToken')
+        expect(setStorageStub.value).toBe(accessToken)
+
+    })
+
+})
