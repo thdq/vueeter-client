01a3f353941ebf2fd711b5b1fef1de4ccf35e9cf
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-05-15 22:23:21 -0300

    feat: init home page

--- src/presentation/pages/home.vue
+++ src/presentation/pages/home.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
+        <vs-row class="h-screen" align="center" justify="space-around" direction="column">
+            <vs-col w="8" class="h-screen">
+                <vs-row>
+                    <vs-col w="3">
+                        <v-sidebar />
+                    </vs-col>
+                </vs-row>
+            </vs-col>
+        </vs-row>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+import Sidebar from '~app/components/shared/sidebar/sidebar.vue'
 
 export default defineComponent({
-    name: "VLogin",
+    components: {
+        'v-sidebar': Sidebar
+    },
+    name: "VHome",
     setup () {
 
         return {
